b633783421fffac6f57ac0f99360a83112a2ee76
noushin94 <noushin_ahmadvand@yahoo.com>
2024-02-08 17:07:36 +0000

    reading mnist

--- MNIST_KNN.py
+++ MNIST_KNN.py
@@ -3,3 +3,9 @@ import pandas as np
 from sklearn.model_selection import train_test_split
 import cv2
 import tensorflow
+from tensorflow import keras
+from keras.datasets import mnist
+
+
+(x_train,y_train) , (x_test, Y_test) = mnist.load_data()
+

5f14528dc0bb0967f9d582bb2197d5a7f7f50d46
noushin94 <noushin_ahmadvand@yahoo.com>
2024-02-12 16:40:59 +0000

    first nueral network with accuracy 82%

---
+++ firstdeeplearning.py
@@ -0,0 +1,91 @@
+import numpy as np
+import pandas as pd
+import cv2
+import glob
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+import joblib
+import tensorflow
+from tensorflow import keras
+from keras import models, layers
+from keras.utils import to_categorical
+import matplotlib.pyplot as plt
+
+
+
+
+features_vectors  = []
+All_labels = []
+
+for i,address in enumerate(glob.glob("/Users/noushinahmadvand/DocumentsCloud/myExercising/brain_Tumor- detection/*/*")):
+
+     img = cv2.imread(address)
+     img = cv2.resize(img, (32,32))
+    
+     img = img/255.0
+     img = img.flatten()
+     
+     features_vectors.append(img)
+
+     #print(address.split("/")[-2])
+     labels = address.split("/")[-2]
+
+     All_labels.append(labels)
+
+     if i % 100 == 0 :
+          print(f"[INFO] {i}/1000 processed")
+
+features_vectors = np.array(features_vectors)          
+
+Label_Encoder = LabelEncoder()
+
+label_encoded =  Label_Encoder.fit_transform(All_labels)
+
+label_encoded = to_categorical(label_encoded )
+
+
+
+
+#print(label_encoded)
+
+# spliting data into train and test
+
+X_train , X_test , Y_train , Y_test = train_test_split(features_vectors, label_encoded, test_size= 0.2)
+
+# defing the network
+
+net = models.Sequential([ 
+                         layers.Dense(300, activation='relu', input_dim = features_vectors.shape[1]),
+                         layers.Dense(40, activation= 'relu'),
+                         layers.Dense(2, activation= 'softmax')
+     
+     ])
+
+
+print(net.summary())
+
+net.compile(optimizer= "SGD",
+          loss= "categorical_crossentropy",
+        metrics = ["accuracy"])
+
+
+h = net.fit(X_train,Y_train, batch_size=32, validation_data= (X_test, Y_test), epochs = 10)
+
+
+plt.plot(h.history["accuracy"], label = "train accuracy" )
+plt.plot(h.history["val_accuracy"], label = "test accuracy")
+
+plt.plot(h.history["loss"], label = "train_accuracy" )
+
+plt.plot(h.history["val_loss"], label = "test accuracy")
+plt.legend()
+plt.xlabel("epoch")
+plt.ylabel("accuracy")
+plt.title("tumor detection")
+plt.show()
+
+
+
+
+
